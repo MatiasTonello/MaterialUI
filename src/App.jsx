f2cde992f0caaf33efe1df1ed185ff5aac173c8f
matiastonello <matias.tonello@gm2dev.com>
2022-07-15 17:11:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+      Hola Mundo!
+    </Button>
+  );
+}
+
+export default App;
